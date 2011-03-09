34385b04f3f9f6b0fcda9864ed9524d0cec7d000
Sruthi Gungidi <sruthi.gungidi@gmail.com>
2011-03-08 22:01:14 -0600

    First Push

--- process_login.php
+++ process_login.php
@@ -1,16 +1,14 @@
 <?
 session_start();
 //Todo: change the settings from local to global
-$server = "localhost";
-$username = "root";
-$password = "root";
-$db_name = "advising";
-
+$server = "omega.uta.edu";
+$username = "snl2898";
+$password = "D-m8T5xy%d";
+$db_name = "User_Info";
 $db = mysql_connect($server,$username,$password) or DIE("Connection to database failed, perhaps the service is down !!");
-mysql_select_db($db_name) or DIE("Database name not available !!");
-
-$login = mysql_query("select * from users where (username = '" . $_POST['myusername'] . "') and (password = '" . md5($_POST['mypassword']) . "')",$db);
-$rowcount = mysql_num_rows($login);
+mysql_select_db($db_name) or DIE("Database name not available !!");P
+$login = mysql_query("select * from users where (Username = '" . $_POST['myusername'] . "') and (Password = '" . md5($_POST['mypassword']) . "')",$db);
+$rowcount = mysql_num_rows($login)P
 
 if ($rowcount == 1) {
 $_SESSION['username'] = $_POST['myusername'];
