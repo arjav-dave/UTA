aeccfcfc99bbda5b831128bafbe4284eace3fa83
Sruthi Gungidi <sruthi.gungidi@gmail.com>
2011-03-10 15:20:15 -0600

    process_welcome.php

--- process_welcome.php
+++ process_welcome.php
@@ -7,18 +7,16 @@ if (!$con)
 
 else{
 
-die("DB connection successful");
-}
 
-mysql_select_db("advising");
+
+mysql_select_db("snl2898");
 $student_profile = mysql_query("SELECT * FROM USER_INFO")
 or die(mysql_error());
+//$row_1 =  mysql_fetch_array($student_profile);
+//echo "Row 1:Username".$row_1['Username'];
 
-echo mysql_fetch_array($student_profile)['Username'];
-
-$student_courses = mysql_query("Select STUDENT_ID,COURSE1,COURSE2,COURSE3,COURSE4,COURSE5,COURSE6,COURSE7,COURSE8,COURSE9,COURSE10,COURSE11,COURSE12 from USERDETAILS_INFO '")    
-or die(mysql_error());
-
+$student_courses = mysql_query("Select STUDENT_ID,COURSE1,COURSE2,COURSE3,COURSE4,COURSE5,COURSE6,COURSE7,COURSE8,COURSE9,COURSE10,COURSE11,COURSE12 from USERDETAILS_INFO") or die(mysql_error());
+}
 mysql_close($con);
 ?>
 
